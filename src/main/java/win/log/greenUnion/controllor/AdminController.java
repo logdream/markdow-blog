f0a4ed01a5bbd7bab14f23f737e832fc1479131f
LOG-SURFACE\surface <fedoraer@gmail.com>
2017-11-24 01:01:46 +0800

    法烦烦

--- src/main/java/win/log/greenUnion/controllor/AdminController.java
+++ src/main/java/win/log/greenUnion/controllor/AdminController.java
@@ -1,7 +1,6 @@
 package win.log.greenUnion.controllor;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.yaml.snakeyaml.constructor.BaseConstructor;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
